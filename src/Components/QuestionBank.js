97747537b6d0ffeda0d329fb3b8e849e789c5afa
Sandeep Pal <sandeeppal6926@gmail.com>
2026-09-14 00:18:10 +0530

    initial commit

--- src/Components/QuestionBank.js
+++ src/Components/QuestionBank.js
@@ -3,4 +3,8 @@ const qBank = [
         options: ["Pune", "Mumbai", "Nagpur", "Nashik"], 
         answer: "Mumbai", 
     },
+    { id: 2, question: "Which city is known as the Pink City of India?", 
+        options: ["Jaipur", "Jodhpur", "Udaipur", "Bikaner"], 
+        answer: "Jaipur", 
+    },
 ]
